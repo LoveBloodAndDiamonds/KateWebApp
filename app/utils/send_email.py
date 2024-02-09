bd0c9311160643d823b9a01b85f0966933558cbe
blowyourbra1n <blowyourbra1n@mail.ru>
2024-02-09 21:45:56 +0300

    Add async support

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -1,36 +1,29 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import smtplib
+import aiosmtplib
 
 from app.config import Config
 
 
 async def send_email(message: str) -> bool:
     """
-    Отправка письма на электронную почту
+    Асинхронная отправка письма на электронную почту
     :param message: Текст сообщения
     :return: bool: Возвращаем True, если сообщение было успешно отправлено, иначе False
     """
     try:
-        # Авторизация на почтовом сервере
-        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server.ehlo()
-        server.login(Config.EMAIL_LOGIN, Config.EMAIL_PASSWORD)
-
         # Создание сообщения
         msg = MIMEMultipart()
         msg['From'] = Config.EMAIL_LOGIN
         msg['To'] = Config.ACCEPTOR_EMAIL
         msg['Subject'] = 'Сообщение о похищении!'
-
         msg.attach(MIMEText(message, 'plain'))
 
-        # Отправка сообщения
-        server.send_message(msg)
-
-        # Закрытие соединения с почтовым сервером
-        server.quit()
+        # Авторизация на почтовом сервере и отправка сообщения
+        async with aiosmtplib.SMTP(hostname="smtp.gmail.com", port=465, use_tls=True) as server:
+            await server.login(Config.EMAIL_LOGIN, Config.EMAIL_PASSWORD)
+            await server.send_message(msg)
 
         return True
     except Exception as e:
--- app/utils/send_telegram.py
+++ app/utils/send_telegram.py
@@ -1,21 +1,19 @@
-import requests
-
+import httpx
 from app.config import Config
-
 
 async def send_telegram_message(message: str) -> bool:
     """
-    Отправляет сообщение в Telegram чат.
+    Асинхронно отправляет сообщение в Telegram чат.
     :param message: Текст сообщения для отправки.
-
     :return: bool: Возвращаем True, если сообщение было успешно отправлено, иначе False
     """
     try:
         url = f"https://api.telegram.org/bot{Config.BOT_TOKEN}/sendMessage"
         payload = {"chat_id": Config.TG_ADMIN_ID, "text": message}
-        response = requests.post(url, data=payload)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, data=payload)
 
-        if not response.ok:
+        if not response.is_success:
             raise Exception(f"Ошибка при отправке сообщения: {response.text}")
 
         return True
